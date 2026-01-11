eb5fc7ee1ed81def79f3a21a9de9c401347e93ba
Luno33 <7282819+Luno33@users.noreply.github.com>
2026-01-11 13:44:02 +0000

    Add gitlog and gittag aliases for prettier git output

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -13,4 +13,8 @@ alias rm="rm -i"
 alias mv="mv -i"
 alias cp="cp -i"
 
+# Git
+alias gitlog='git log --all --graph --oneline --format="%C(yellow)%h %C(bold blue)%an %C(green)%ad %C(yellow)%d %C(reset)%s" --date=relative'
+alias gittag='git tag --sort=-creatordate --format="%(color:yellow)%(refname:short)%(color:reset) - %(color:green)%(creatordate:short)%(color:reset) - %(color:bold blue)%(taggername)%(color:reset) - %(contents:subject)"'
+
 # Add your aliases below
